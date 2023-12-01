6b499927363a254dfa1d36ad5b9f561d1b9c0b88
Jay Raval <70907419+jayr1867@users.noreply.github.com>
2023-12-01 16:21:00 -0600

    refactoring

--- index.js
+++ index.js
@@ -34,17 +34,6 @@ const io = sockio(server, {
 
 
 
-
-// const io = new Server( app.listen(port, () => {
-//   console.log("listening on port: " + port);
-// }),{
-//   cors: {
-//     origin: 'http://127.0.0.1:5173',
-//     methods: ["GET", "POST"],
-//   }
-// });
-
-
 const speechClient = new speech.SpeechClient();
 
 const rooms = {};
@@ -62,23 +51,6 @@ io.on('connection', (socket) => {
     }
 
     rooms[roomName].push({ STT: data.sttLang, fromLang: data.transLang, toLang:'', socketId: socket.id });
-
-    // if (rooms[roomName].length === 2) {
-    // socket.emit("counter", rooms[roomName].length);
-    // }
-
-    console.log(rooms);
-
-    // delete (rooms[roomName]);
-
-    // console.log(rooms);
-
-    const cli = io.sockets.adapter.rooms.get(data.roomName);
-
-    // console.log(cli.size);
-
-
-    // socket.emit('query', data);
   });
 
   //send message to client
@@ -104,29 +76,30 @@ io.on('connection', (socket) => {
     console.log("** ending google cloud stream **\n");
     stopRecognitionStream();
 
-    if (rooms[data.roomName]) {
-      rooms[data.roomName] = rooms[data.roomName].filter((client) => {
-        if (client.socketId === socket.id) {
-          return false; // Remove the disconnected client
-        }
-        return true;
-      });
+    if (rooms[data]) {
+      if (rooms[data].length === 2) {
+        rooms[data] = rooms[data].filter((client) => {
+          if (client.socketId === socket.id) {
+            return false; // Remove the disconnected client
+          }
+          return true;
+        });
+      } else {
+        delete (rooms[data]);
+      }
     }
-    // if (!rooms[data.roomName]) {
-    //   console.log("Room is empty");
-    // }
-    // console.log(rooms)
   });
 
   socket.on("send_audio_data", async (audioData) => {
     io.emit("receive_message", "Got audio data");
-    if (recognizeStream !== null) {
+    if (recognizeStream !== null && !recognizeStream.destroyed) {
       try {
         recognizeStream.write(audioData.audio);
       } catch (err) {
         console.log("Error calling google api " + err);
       }
     } else {
+      socket.emit("receive_message", "RecognizeStream is null");
       console.log("RecognizeStream is null");
     }
   });
@@ -150,33 +123,15 @@ io.on('connection', (socket) => {
       interimResults: true,
     };
 
-    console.log(request);
-
-    // for (const client of rooms[roomName]) {
-    //   if (client.socketId !== socket.id) {
-    //     toLang = client.fromLang;
-    //     break;
-    //   }
-    // }
-
-    // if (!toLang) {
-    //   return;
-    // }
 
     if (rooms[roomName].length === 2) {
-      // console.log(toLang);
-      // console.log(rooms[roomName]);
       rooms[roomName][0].toLang = rooms[roomName][1].fromLang;
       rooms[roomName][1].toLang = rooms[roomName][0].fromLang;
-      
     }
-
-    console.log(rooms);
 
 
 
     try {
-      // console.log('inside!');
       recognizeStream = speechClient
         .streamingRecognize(request)
         .on("error", console.error)
@@ -191,46 +146,35 @@ io.on('connection', (socket) => {
           console.log(`Transcription: `, transcription);
 
           let clientid;
-
-          if (rooms[roomName].length === 2) {
-            for (const client of rooms[roomName]) {
-              if (client.socketId === socket.id) {
-                fromLang = client.fromLang;
-                toLang = client.toLang;
-                // break;
-              } else {
-                clientid = client.socketId;
+          if (rooms[roomName]) {
+            if (rooms[roomName].length === 2) {
+              for (const client of rooms[roomName]) {
+                if (client.socketId === socket.id) {
+                  fromLang = client.fromLang;
+                  toLang = client.toLang;
+                  // break;
+                } else {
+                  clientid = client.socketId;
+                }
               }
+              const options = {
+                from: fromLang,
+                to: toLang,
+              };
+              
+              console.log(options)
+
+              const trans = await translate.translate(transcription, options);
+
+              io.to(clientid).emit("receive_audio_text", {
+              
+              // client.emit("receive_audio_text", {
+                text: trans[0],
+                // text: transcription,
+                final: isFinal,
+              });
             }
-            const options = {
-              from: fromLang,
-              to: toLang,
-            };
-            
-            console.log(options)
-
-            const trans = await translate.translate(transcription, options);
-
-            io.to(clientid).emit("receive_audio_text", {
-            
-            // client.emit("receive_audio_text", {
-              text: trans[0],
-              // text: transcription,
-              final: isFinal,
-            });
-          } else {
-
-
-          // console.log(transcription);
-
-          // console.log(trans[1].data.fromLangs[0].tranlateText )
-
-          // client.emit("receive_audio_text", {
-          //   // text: trans[0],
-          //   text: transcription,
-          //   final: isFinal,
-          // });
-        }
+          }
         });
     } catch (err) {
       console.error("Error streaming google api " + err);
@@ -245,8 +189,6 @@ io.on('connection', (socket) => {
     recognizeStream = null;
   }
 });
-
-
 
 
 app.post("/join-room", async (req, res) => {
@@ -266,9 +208,6 @@ app.post("/join-room", async (req, res) => {
 });
 
 
-
 const encoding = "LINEAR16";
 const sampleRateHertz = 16000;
-// const languageCode = "en-US"; //en-US
-const alternativeLanguageCodes = ["en-US", "ko-KR"];
 
